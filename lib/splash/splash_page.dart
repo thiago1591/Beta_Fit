d3a4ab8557677b0701c1235d4339f9968fc33bff
Thiago <thiagoandre2121@gmail.com>
2021-08-14 15:20:35 -0300

    Created splash screen

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:debate_place_flutter/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
@@ -7,7 +8,21 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/texture.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/logo.png'),
+            Padding(padding: EdgeInsets.all(25),
+            child: Text('Por uma comunidade dev mais saudável',style: AppTextStyles.splashTitle,textAlign: TextAlign.center),)
+        ],) 
       ),
     );
   }
