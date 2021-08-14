4392c72d12729fe159dcb85edc116a79792307c6
Thiago <thiagoandre2121@gmail.com>
2021-08-14 15:22:07 -0300

    Created login page

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,15 +1,45 @@
+import 'package:debate_place_flutter/core/app_text_styles.dart';
+import 'package:debate_place_flutter/login/widgets/button_widget.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({ Key? key }) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(children: [
-          Text('Login Page')
-        ],),
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/login-background.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Image.asset('assets/images/logo2.png'),
+                SizedBox(
+                  height: 20,
+                ),
+                Text('Por uma comunidade',
+                    style: AppTextStyles.loginTitle,
+                    textAlign: TextAlign.center),
+                Text('dev mais saudável',
+                    style: AppTextStyles.loginTitle,
+                    textAlign: TextAlign.center),
+                Expanded(flex: 1,child: Text(''),),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child:LoginButtonWidget()
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
