61e15e8c3e0ab426414f7e42de1425c022fb409c
Thiago <thiagoandre2121@gmail.com>
2021-08-16 21:01:06 -0300

    Created image card

--- lib/home/widgets/imagecard/image_card_widget.dart
+++ lib/home/widgets/imagecard/image_card_widget.dart
@@ -1,16 +1,53 @@
 import 'package:flutter/material.dart';
 
 class ImageCardWidget extends StatelessWidget {
-  const ImageCardWidget({Key? key}) : super(key: key);
+  final String photoURL,date;
+  const ImageCardWidget({Key? key, required this.photoURL, required this.date}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-          padding: EdgeInsets.symmetric(horizontal: 10),
-          child: Column(children: [
-          Image.asset('assets/images/running.jpg'),
-          Text('10/08')
-          ],)
-        );
+      padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+      child: Column(
+        children: [
+          Expanded(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10)),
+              ),
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage('$photoURL'),fit: BoxFit.cover
+                        ),
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5))
+                      ),
+                    ),
+                  ),
+                  
+                  Expanded(
+                      flex: 1,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '16/08',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ))
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
