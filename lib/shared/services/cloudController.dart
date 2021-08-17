a834014fd8eb5720ac556d3e2def5f88981cdc1c
Thiago <thiagoandre2121@gmail.com>
2021-08-16 21:03:35 -0300

    Created cloudController

---
+++ lib/shared/services/cloudController.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:debate_place_flutter/shared/models/user_model.dart';
+
+Future<void> setUserCloud(UserModel user) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+      return users
+      .doc(user.name)
+          .set({
+            'name': user.name,
+            'photoURL': user.photoURL, 
+            'imagesQtt': user.imagesQtt 
+          })
+          .then((value) => print("User Added"))
+          .catchError((error) => print("Failed to add user: $error"));
+}
+
+Future<void> updateImageQtt(String user, qtt) {
+
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+          final imagesQtt = users.doc(user).get();
+          print(imagesQtt);
+         return users
+          .doc(user)
+          .update({
+            'imagesQtt': qtt+1
+          })
+          .then((value) => print("User Updated"))
+          .catchError((error) => print("Failed to add user: $error"));
+}
+
+Future<void> readUsers(UserModel user) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+      return users
+          .doc(user.name)
+          .set({
+            'name': user.name, 
+            'photoURL': user.photoURL, 
+            'imagesQtt': user.imagesQtt 
+          })
+          .then((value) => print("User Added"))
+          .catchError((error) => print("Failed to add user: $error"));
+}
